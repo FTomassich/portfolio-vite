5b5b751c459c38685491b8b719443ec1da679112
Franco Tomassich <francotomassich@gmail.com>
2024-05-24 18:50:55 -0300

    hola

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,11 +8,14 @@ const Hero = () => {
   return (
     <section className={styles.container}>
         <div className={styles.content}>
-            <h1 className={styles.title}>Hi, I'm Franco</h1>
-        <p className={styles.description}>I'm a full stack developer with experience in React, Node Js, Express, PostgreSQL🚀</p>
+            <h1 className={styles.title}>Franco Tomassich</h1>
+        <p className={styles.description}>Full stack developer with experience in HTML, CSS, React, Redux, Express, Sequelize, PostgreSQL, 🚀</p>
         <a href="mailto:francotomassich@gmail.com" className={styles.contactBtn}>Contact me</a>
         </div>
         <img src={me} alt='Hero image of me' className={styles.heroImg}/>
+        
+        
+     
         <div className={styles.topBlur}/>
         <div className={styles.bottonBlur}/>
    
