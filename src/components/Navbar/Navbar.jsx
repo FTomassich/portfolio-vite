d1b05082930aa6d23c74009f2a36decdd9226d43
Franco Tomassich <francotomassich@gmail.com>
2024-05-26 06:55:43 -0300

    h

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 const [menuOpen, setMenuOpen]=useState(false)
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">Portfolio</a>
+      <a className={styles.title} href="/">My web</a>
       <div className={styles.menu}>
         <img className={styles.menuBtn} 
         src={
