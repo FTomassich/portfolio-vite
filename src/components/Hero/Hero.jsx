5533efe0ed150d9ff456ffbc065796dd3baaff43
Franco Tomassich <francotomassich@gmail.com>
2024-06-01 20:37:22 -0300

    hola

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,12 +2,14 @@
 import styles from './Hero.module.css';
 
 import atron from "../../assets/atron.png";
+import fondo from "../../assets/fondo.jpg"
 
 
 
 const Hero = () => {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} style={{ backgroundImage: `url(${fondo})` }}>
+      
         <div className={styles.content}>
             <h1 className={styles.title}>Franco Tomassich</h1>
         <p className={styles.description}>Full stack developer with experience in HTML, CSS, React, Redux, Express, Sequelize, PostgreSQL, 🚀</p>
