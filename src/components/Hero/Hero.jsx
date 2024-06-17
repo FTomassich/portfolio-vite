40458367ebbc5dc8b4f4def53500c075c683f6cd
Franco Tomassich <francotomassich@gmail.com>
2024-06-17 04:50:06 -0300

    proba

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ import fondo from "../../assets/fondo.jpg"
 const Hero = () => {
   return (
     <section className={styles.container} style={{ backgroundImage: `url(${fondo})` }}>
- {/* <div className={styles.content}>
+ <div className={styles.content}>
         <h1 className={styles.title}>Franco Tomassich</h1>
         <p className={styles.description}>Full stack developer with experience in HTML, CSS, React, Redux, Express, Sequelize, PostgreSQL, 🚀</p>
       </div>
@@ -17,7 +17,7 @@ const Hero = () => {
       <div className={styles.contImage}>
         <img src={atron} alt='Hero image of me' className={styles.heroImg} />
         <a href="mailto:francotomassich@gmail.com" className={styles.contactBtn}>Contact me</a>
-      </div> */}
+      </div>
      
       
     </section>
