97c52c98996328da15746da1595470f489fe79b3
Franco Tomassich <francotomassich@gmail.com>
2024-06-23 22:43:54 -0300

    nnn

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import styles from "./App.module.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Skills from "./components/Skills/Skills";
+import About from "./components/About/About";
 
 function App() {
 
   return (
     <>
      <div className={styles.App}>
+     
       <Navbar/>
       <Hero/>
+      <About/>
       <Skills/>
+      
      </div>
     </>
   )
