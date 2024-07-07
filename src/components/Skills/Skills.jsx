5fc70a02555eeff855bdce61c26f254505bd457e
Franco Tomassich <francotomassich@gmail.com>
2024-07-07 20:36:16 -0300

    uuii

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -18,7 +18,7 @@
 //       <div>
 //         <h1 className={styles.title}> Tech Skills</h1>
 //         </div>
-        
+
 //           <ul className={styles.techList}>
 //             <li><img src={react} alt="" className={styles.icon}/></li>
 //             <li><img src={github} alt="" className={styles.icon} /></li>
@@ -30,9 +30,9 @@
 //             <li><img src={git} alt="" className={styles.icon} /></li>
 //             <li><img src={vite} alt="" className={styles.icon} /></li>
 //           </ul>
-       
+
 //     </section>
-    
+
 //   )
 // }
 
@@ -75,33 +75,35 @@ const Skills = () => {
         <h1 className={styles.title}>Tech Skills</h1>
       </div>
       <div className={styles.carousel}>
-      <ul className={styles.techList}>
-        {icons.concat(icons).map((icon, index) => (
-          <li key={index}>
-            <img src={icon.src} alt={icon.alt} className={styles.icon} />
-          </li>
-        ))}
-      </ul>
+        <ul className={styles.techList}>
+          {icons.concat(icons).map((icon, index) => (
+            <li key={index}>
+              <img src={icon.src} alt={icon.alt} className={styles.icon} />
+            </li>
+          ))}
+        </ul>
       </div>
       <div className={styles.container}>
         <div className={styles.item}>
           <h2 className={styles.h2}>FRONT-END</h2>
           <h3 className={styles.h3}>HTML / CSS / REACT / REDUX</h3>
           <strong className={styles.strong}>
-  Como desarrollador frontend, disfruto creando interfaces de usuario intuitivas y creativas con HTML y React.<br />
-  Tengo experiencia en:<br />
-  Me esfuerzo para que mis sitios sean responsivos y se adapten a distintos dispositivos. Tengo experiencia en desarrollar componentes avanzados como formularios controlados para creación de productos, inicio de sesión, registro de usuarios y recuperación de contraseñas.<br />
-  Utilizo Redux para la gestión del estado y estilizo componentes con CSS, Bootstrap y preprocesadores como Less.<br />
-  También tengo experiencia en la integración de APIs RESTful, la implementación de operaciones asíncronas con JavaScript, y la gestión de contenido multimedia con Cloudinary.<br />
-  Mi enfoque combina estética y funcionalidad para lograr productos web atractivos y altamente usables.
-</strong>
-        
+            Como desarrollador frontend, disfruto creando interfaces de usuario intuitivas y creativas con HTML y React.<br />
+            Tengo experiencia en:<br />
+            Desarrollar componentes avanzados como formularios controlados para creación de productos, inicio de sesión, registro de usuarios y recuperación de contraseñas.<br />
+            Diseños web responsivos estilizados con CSS y preprocesadores como Less.<br />
+            Empleo de Redux para la gestión del estado<br />
+            Integración de APIs RESTful, con implementación de operaciones asíncronas con JavaScript<br />
+            Gestión de contenido multimedia con Cloudinary.<br />
+            Mi enfoque combina estética y funcionalidad para lograr productos web atractivos y altamente usables.
+          </strong>
+
         </div>
         <div className={styles.item}>
           <h2 className={styles.h2}>BACK-END</h2>
           <h3 className={styles.h3}>EXPRESS/POSTGRESQL/SEQUELIZE</h3>
           <strong className={styles.strong}>Tengo experiencia en la diagramación y gestión de bases de datos eficientes con PostgreSQL. He desarrollado servidores utilizando Express.js para aplicaciones Node.js y tengo habilidades en el mapeo objeto-relacional con Sequelize ORM. Esto incluye la creación de relaciones entre tablas y la implementación de índices para mejorar el rendimiento. Además, tengo experiencia en la integración de APIs y la realización de fetching de datos. Esta combinación de habilidades me permite desarrollar soluciones backend robustas que soportan aplicaciones complejas y de alto rendimiento.</strong>
-        
+
         </div>
       </div>
     </section>
