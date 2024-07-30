acc45b26e869dca4ee649b080196db9a30cf494d
Franco Tomassich <francotomassich@gmail.com>
2024-07-30 16:54:54 -0300

    fffff

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <section className={styles.container} style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.1), rgba(0, 0, 0, 0.1)), url(${about})` }}>
             <div className={styles.duo}>
                 <h1 className={styles.title}>About</h1>
-                <h2>🖐Hola! Me da gusto recibirte en mi portfolio. En estas slides te comentaré brevemente acerca de mi perfil profesional</h2>
+                <h2 className={styles.subt}>🖐Hola! Me da gusto recibirte en mi portfolio. En estas slides te comentaré brevemente acerca de mi perfil profesional</h2>
                 {/* Puedes agregar más contenido aquí si es necesario */}
             </div>
             <CustomSlider />
