6d1e00e57a041d4513ac66877f5b88e018629a22
Franco Tomassich <francotomassich@gmail.com>
2024-08-03 20:40:55 -0300

    jjjj

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './About.module.css';
 import CustomSlider from '../Slider/Slider';
-import stars from "../../assets/stars.png"
+import spacex from "../../assets/spacex.jpg"
 
 const About = () => {
     return (
-        <section className={styles.container} style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.1), rgba(0, 0, 0, 0.1)), url(${stars})` }}>
+        <section className={styles.container} style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.1), rgba(0, 0, 0, 0.1)), url(${spacex})` }}>
             <div className={styles.duo}>
                 <h1 className={styles.title}>About</h1>
                 <h2 className={styles.subt}>🖐Hola! Me da gusto recibirte en mi portfolio. En estas slides te comentaré brevemente acerca de mi perfil profesional</h2>
