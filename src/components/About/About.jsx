7a8b8c30b09a77b3cba5d4f57c477f26ff3fe54e
Franco Tomassich <francotomassich@gmail.com>
2024-08-25 18:55:11 -0300

    jijiji

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './About.module.css';
 import CustomSlider from '../Slider/Slider';
-import spacex from "../../assets/spacex.jpg"
+import space12 from "../../assets/space12.jpg"
 
 const About = () => {
     return (
-        <section className={styles.container} style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.1), rgba(0, 0, 0, 0.1)), url(${spacex})` }}>
+        <section className={styles.container} style={{ 
+            backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 1) 90%),linear-gradient(to top, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 1) 90%), 
+             url(${space12})
+            ` }}>
             <div className={styles.duo}>
                 <h1 className={styles.title}>About</h1>
                 <h2 className={styles.subt}>🖐Hola! Me da gusto recibirte en mi portfolio. En estas slides te comentaré brevemente acerca de mi perfil profesional</h2>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,12 +3,14 @@ import styles from './Hero.module.css';
 
 import atron from "../../assets/atron.png";
 import fondo from "../../assets/fondo.jpg"
+import spacex from "../../assets/spacex.jpg"
 
 
 
 const Hero = () => {
   return (
-    <section className={styles.container} style={{ backgroundImage: `url(${fondo})` }}>
+    <section className={styles.container} style={{ 
+      backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 1) 90%), url(${spacex})`}}>
       <div className={styles.content}>
         <h1 className={styles.title}>Franco Tomassich</h1>
         <p className={styles.description}>Desarrollador Full stack con experiencia en React, Redux, Express, Sequelize, PostgreSQL 🚀</p>
