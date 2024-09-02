a7a83f4c510d2eaa48c24d9c6a779db40d7ec364
Franco Tomassich <francotomassich@gmail.com>
2024-09-02 03:58:41 -0300

    kkk

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,11 +5,26 @@ import space2020 from "../../assets/space2020.jpg"
 
 const Projects = () => {
   return (
-    <section className={styles.section} style={{ 
-        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 1) 90%),linear-gradient(to top, rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 1) 90%), radial-gradient(ellipse at center, rgba(0, 0, 0, 0) 30%, rgba(0, 0, 0, 1) 80%), 
+    <section className={styles.section} style={{
+      backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 1) 90%),linear-gradient(to top, rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 1) 80%), radial-gradient(ellipse at center, rgba(0, 0, 0, 0) 30%, rgba(0, 0, 0, 1) 80%), 
                           url(${space2020})
         `  }}>
-        <h1>Projects</h1>
+      <h1 className={styles.title}>Projects</h1>
+      <div className={styles.container}>
+        <div className={styles.item}>
+          1
+        </div>
+        <div className={styles.item}>
+          2
+        </div>
+        <div className={styles.item}>
+          3
+        </div>
+        <div className={styles.item}>
+          4
+        </div>
+
+      </div>
 
     </section>
   )
