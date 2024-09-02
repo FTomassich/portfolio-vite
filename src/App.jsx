8abab34564dcbed021d8b208ea37f94cecca67dc
Franco Tomassich <francotomassich@gmail.com>
2024-09-01 22:00:17 -0300

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Skills from "./components/Skills/Skills";
 import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 
 function App() {
 
   return (
     <>
      <div className={styles.App}>
-     
       <Navbar/>
       <Hero/>
       <About/>
       <Skills/>
-      
+      <Projects/>
      </div>
     </>
   )
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../Projects/Projects.module.css';
+
+const Projects = () => {
+  return (
+    <section className={styles.container}>
+        <h1>Projects</h1>
+
+    </section>
+  )
+}
+
+export default Projects
