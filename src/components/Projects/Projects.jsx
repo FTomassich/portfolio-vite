d184e4b6ae0d1cb5109908cdf29e4fcf46a4965b
Franco Tomassich <francotomassich@gmail.com>
2024-09-01 22:20:44 -0300

    hjhjhj

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from '../Projects/Projects.module.css';
+import space2020 from "../../assets/space2020.jpg"
+
 
 const Projects = () => {
   return (
-    <section className={styles.container}>
+    <section className={styles.section} style={{ 
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 1) 90%),linear-gradient(to top, rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 1) 90%), radial-gradient(ellipse at center, rgba(0, 0, 0, 0) 30%, rgba(0, 0, 0, 1) 80%), 
+                          url(${space2020})
+        `  }}>
         <h1>Projects</h1>
 
     </section>
