30d0a9d218c96c69c7ab9628035fd8ce9e8d0a29
Franco Tomassich <francotomassich@gmail.com>
2024-09-12 09:48:35 -0300

    frfrfrf

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,10 +22,11 @@ const [menuOpen, setMenuOpen]=useState(false)
           />
         <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
           onClick={()=> setMenuOpen(false)}>
-          <li><a href="#about">About</a></li>
-          <li><a href="#skills">Skills</a></li>
+            <li><a href="#home">Home</a></li>
+          <li><a href="#about">About me</a></li>
+          <li><a href="#skills">Tech Skills</a></li>
           <li><a href="#projects">Projects</a></li>
-          <li><a href="#about">Contact</a></li>
+          
         </ul>
       </div>
     </nav>
