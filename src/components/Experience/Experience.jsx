26f01563608ab8de09396fb471092941b8eb95a5
Franco Tomassich <francotomassich@gmail.com>
2025-03-10 17:00:18 -0300

    salto

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -26,8 +26,8 @@ const TimelineItem = ({ title, date, description, tasks, icon: Icon }) => (
 export default function Timeline() {
   return (
     <div className="max-w-4xl mx-auto px-4 py-12">
-      <h2 className="text-5xl font-extrabold text-center mb-12 text-white-700 py-10 leading-[2.5rem]">
-  Experiencia Profesional
+      <h2 className="text-5xl font-extrabold text-center mb-12 text-white-700 py-10">
+  Experiencia<br />Profesional
 </h2>
 
       <div className="space-y-8">
