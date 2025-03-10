ac8af8d9de6fc4dd19516b3716f4b0fd2c52d0f1
Franco Tomassich <francotomassich@gmail.com>
2025-03-10 17:08:11 -0300

    h1

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -26,9 +26,9 @@ const TimelineItem = ({ title, date, description, tasks, icon: Icon }) => (
 export default function Timeline() {
   return (
     <div className="max-w-4xl mx-auto px-4 py-12">
-      <h2 className="text-5xl font-extrabold text-center mb-12 text-white-700">
+      <h1 className="text-5xl font-extrabold text-center mb-12 text-white-700">
   Experiencia 
-  </h2>
+  </h1>
   
 
       <div className="space-y-8">
