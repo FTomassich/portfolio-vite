5e7b4b5d3f1df389893a14af597c00bd4cbebabd
Franco Tomassich <francotomassich@gmail.com>
2025-03-10 17:13:05 -0300

    chan

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     return (
         <section id="about" className={styles.container} >
             <div className={styles.duo}>
-                <h1 className={styles.title}>Sobre mí</h1>
+                <h1 className="text-5xl font-extrabold text-center mb-12 text-white-700">Sobre mí</h1>
                 <p className={styles.subt}>🖐😀Hola! Me da gusto recibirte en mi portfolio. En estas slides te comentaré brevemente acerca de mi perfil profesional</p>
                 
             </div>
