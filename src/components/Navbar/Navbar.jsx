5b6b8f6df683ff6d0a27f02b3f9792888d2bbf18
Franco Tomassich <francotomassich@gmail.com>
2025-07-11 14:57:18 -0300

    vinculo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,6 +26,7 @@ const Navbar = () => {
           <li><a href="#about">Sobre mí</a></li>
           <li><a href="#skills">Habilidades</a></li>
           <li><a href="#projects">Proyectos</a></li>
+          <li><a href="#experience">Experiencia</a></li>
 
         </ul>
       </div>
