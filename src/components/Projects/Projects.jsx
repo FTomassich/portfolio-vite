12e02813cc27c62910b74ff016ba50db03b19781
Franco Tomassich <francotomassich@gmail.com>
2025-07-18 17:11:12 -0300

    changes 07/2025

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,11 +3,12 @@ import styles from '../Projects/Projects.module.css';
 
 import gitt from "../../assets/gitt.png";
 import go from '../../assets/go.png';
-import dogsi from '../../assets/dogsi.png';
+import person from '../../assets/person.svg';
 import planet from '../../assets/planet.png';
-import dog from '../../assets/dog.png';
+import social from '../../assets/social.svg';
 import cart from '../../assets/cart.png';
-import selectoras from '../../assets/selectoras.png'
+import edit from '../../assets/edit.png'
+import proyecto from '../../assets/proyecto.png'
 
 const Projects = () => {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -26,33 +27,28 @@ const Projects = () => {
           <div className={styles.innerItem}>
             <div className={styles.front}>
               <div className={styles.group2}>
-                <img className={styles.btn} src={dog} alt="" />
-                <h1 className={styles.sub}>Find Dogs App</h1>
+                <img className={styles.btn} src={person} alt="" />
+                <h1 className={styles.sub}>Quality App</h1>
           
               </div>
-              <span className={styles.txt}>WEB DEVELOPMENT</span>
-              <img src={dogsi} className={styles.imgDogs} alt="Find Dogs App" />
+              <span className={styles.txt}>APP-GESTIÓN HUMANA</span>
+              <img src={edit} className={styles.imgDogs} alt="Find Dogs App" />
               <button className={styles.seeMoreBtn} onClick={() => handleButtonClick(0)}>
                 Ver más
               </button>
             </div>
             <div className={styles.back}>
-            <p className={styles.description}>Una app en la que podrás buscar perros por raza, ordenarlos y filtrarlos según características adicionales como temperamento, peso y estatura. Además podrás crear nuevos registros en la base de datos con el formulario controlado. Visítala y conoce a tu próximo amigo de cuatro patas. Tecnologías implicadas en el desarrollo: React, Redux, PostgreSQL, Express js, Sequelize.</p>
+            <p className={styles.description}>La APP de Gestión Humana de Quality es una herramienta administrativa funcional y responsiva, diseñada principalmente para iOS, que facilita la gestión, contratación y registro de empleados en empresas constructoras. Stack técnico utilizado: Node, Typescript, React Native, Redux Tool-Kit, Swagger, Postman</p>
 
 
 
               <div className={styles.flex}>
-              <a href="https://github.com/FTomassich/PI-Dogs">
-                <div className={styles.group}>
-                  <img className={styles.btn} src={gitt} alt="" />
-                  <p>GITHUB</p>
-                </div>
-              </a>
+              
              
-              <a href="https://pi-dogs-sage-three.vercel.app/">
+              <a href="https://drive.google.com/drive/folders/1aob1a3MMSyY6ZA33joAHBKs1QQkBjgLW?usp=drive_link">
                 <div className={styles.group}>
                   <img className={styles.btn} src={planet} alt="" />
-                  <p>DEPLOY</p>
+                  <p>Link </p>
                 </div>
               </a>
               </div>
@@ -72,14 +68,14 @@ const Projects = () => {
                 <img className={styles.btn} src={cart} alt="" />
                 <h1 className={styles.sub}>Innova-tech</h1>
               </div>
-              <span className={styles.txt}>WEB DEVELOPMENT</span>
+              <span className={styles.txt}>E-COMMERCE</span>
               <img src={go} alt="Innova-tech e-commerce" className={styles.img} />
               <button className={styles.seeMoreBtn} onClick={() => handleButtonClick(1)}>
                 Ver más
               </button>
             </div>
             <div className={styles.back}>
-              <p className={styles.description}>Innova-Tech es una moderna plataforma e-commerce de productos tecnológicos, que ofrece a sus clientes una experiencia de compra agradable, fluida, eficiente y segura. Algunas tecnologías desplegadas en su desarrollo fueron: React, Redux, Cloudinary, PostgreSQL, Express, Sequelize, JsonWebToken, 0auth, Mercadopago.</p>
+              <p className={styles.description}>Innova-Tech es una moderna plataforma e-commerce de productos tecnológicos, que ofrece a sus clientes una experiencia de compra agradable, fluida, eficiente y segura. Algunas tecnologías desplegadas en su desarrollo fueron: Node, React, Redux, Cloudinary, PostgreSQL, Express, Sequelize, JsonWebToken, 0auth, Mercadopago, Insomnia, Postman, Trello.</p>
               
               <div className={styles.flex}>
               <a href="https://github.com/Vrmoya/innovatech-front">
@@ -106,23 +102,24 @@ const Projects = () => {
           <div className={styles.innerItem}>
             <div className={styles.front}>
               <div className={styles.group2}>
-                
-                <h1 className="text-xl font-bold text-gray-700 mb-8 last:mb-0 bg-transparent  rounded-lg shadow-md p-3">Otaku Zone Foro</h1>
+                 <img className={styles.btn} src={social} alt="" />
+                <h1 className={styles.sub}>O-Zone</h1>
               </div>
-              <span className={styles.txt}>DESARROLLO WEB</span>
-              <img src={selectoras} alt="Innova-tech e-commerce" className={styles.imgSelec} />
+              <span className={styles.txt}>FORO INTERACTIVO</span>
+              <img  src={proyecto}alt="Innova-tech e-commerce" className={styles.imgDogs} />
               <button className={styles.seeMoreBtn} onClick={() => handleButtonClick(2)}>
                 Ver más
               </button>
             </div>
             <div className={styles.back}>
-              <p className={styles.description}>Proyecto de la empresa CyberSinn Solutions que consistió en un foro de discusión con formato de red social.  </p>
+              <p className={styles.description}>El proyecto O-Zone de la empresa CyberSinn Solutions consistió en el desarrollo de un foro de discusión diseñado para conectar a usuarios apasionados por la cultura del cómic, el animé, el K-pop y otros intereses relacionados. Stack Técnico utilizado: Node, React, Typescript, Tailwind CSS. Express, MongoDB, Trello, Postman.</p>
               
               <div className={styles.flex}>
-              <a >
+              <a href="https://drive.google.com/file/d/16Wa81EoFkSZst2wZ2XLA1mLcU--Sx0b4/view?usp=drive_link">
                 <div className={styles.groupx}>
                   <img className={styles.btn} src={planet} alt="" />
                   <p>LINK</p>
+                  
                 </div>
               </a>
               
