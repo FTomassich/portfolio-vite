ba7a3499a0dbdc1ca2a71032c23154456cc4ec81
Franco Tomassich <francotomassich@gmail.com>
2025-07-18 17:51:08 -0300

    ggggg

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -32,7 +32,7 @@ const Projects = () => {
           
               </div>
               <span className={styles.txt}>APP-GESTIÓN HUMANA</span>
-              <img src={edit} className={`${styles.imgSelec} px-6 md:px-0`} alt="Find Dogs App" />
+              <img src={edit} className={`${styles.imgSelec} px-8 md:px-0`} alt="Find Dogs App" />
               <button className={styles.seeMoreBtn} onClick={() => handleButtonClick(0)}>
                 Ver más
               </button>
