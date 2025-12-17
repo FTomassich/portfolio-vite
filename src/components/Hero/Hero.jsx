4636462bed5bd4ffbbec70d9cac682eb831d3f04
Franco Tomassich <francotomassich@gmail.com>
2025-12-17 19:05:28 -0300

    sdjlajdkas

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 
 import styles from './Hero.module.css';
 import gitt from "../../assets/gitt.png"
-import atron1 from "../../assets/atron1.png";
+import ft from "../../assets/ft.png";
 import mail from "../../assets/mail.png";
 import linkedin from "../../assets/linkedin.png";
 
@@ -13,14 +13,16 @@ const Hero = () => {
     <section id="home" className={styles.container} >
       
       <div className={styles.content}>
-       <p className={styles.name}>Franco Tomassich</p>
-
+       <div>
+        <p className={styles.name}>Franco</p>
+       <p className={styles.name2}>Tomassich</p>
+       </div>
         <p className={styles.description}>Desarrollador web Full stack con background en producción de contenidos</p>
       </div>
 
       <div className={styles.contImage}>
         <div className={styles.box}>
-        <img src={atron1} alt='Hero image of me' className={styles.heroImg} />
+        <img src={ft} alt='Hero image of me' className={styles.heroImg} />
         </div>
 <p className={styles.connect}>💡¿Conectamos ideas? ¡Estoy a solo un click de distancia!</p>
         
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import "@fontsource/outfit"
-import "@fontsource/roboto"
-import "@fontsource/inter"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
